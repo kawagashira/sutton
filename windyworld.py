6a21aecd1db5276cf67452a09321ae4eed85fa43
kawagashira <kawagashira@gmail.com>
2020-08-17 12:36:05 +0900

    show mean step

--- windyworld.py
+++ windyworld.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 import smooth
 
+DIRECTION = {0: 'U', 1:'R', 2:'D', 3:'L'}
+
 class Env:
 
     def __init__(self):
@@ -42,8 +44,6 @@ move    0:north, 1:east, 2:south, 3:west
             x, y = -1, 0
         self.state[0] += x
         self.state[1] += y + self.wind[state0[0]]   # plus wind
-        #print (self.wind[state0[0]])
-        #self.state[1] += y
         r = -1
         is_goal = False
         if self.state == self.goal:
@@ -109,24 +109,23 @@ def sarsa(env, agent, alpha, gamma):
     ### SARSA ###
     env.reset()
     a = agent.e_greedy(env.state)
+    a_list = []
     r = -1
     R = 0
     i = 0
     is_goal = False
-    #while r != 0:        # AN EPISODE
     while not is_goal:        # AN EPISODE
+        a_list.append(a)
         i += 1
         s0 = env.state.copy()
         r, is_goal = env.act(a)
         R += r
         a1 = agent.e_greedy(env.state)
-        #print ('Q(s,a)', agent.get_q(s0, a))
         value = agent.get_q(s0, a)
         agent.q[s0[0], s0[1], a] = agent.get_q(s0, a) + alpha * (r + gamma * agent.get_q(env.state, a1) - agent.get_q(s0, a))
         a = a1
-        if is_goal:
-            break
-    return i, R
+    a_list.append(a)
+    return i, R, ''.join([DIRECTION[a] for a in a_list])
 
 def q_learn(env, agent, alpha, gamma):
 
@@ -136,7 +135,6 @@ def q_learn(env, agent, alpha, gamma):
     i = 0
     r = -1
     is_goal = False
-    #while r != 0:
     while not is_goal:
         i += 1
         s0 = env.state.copy()
@@ -145,9 +143,6 @@ def q_learn(env, agent, alpha, gamma):
         R += r
         value = agent.q[s0[0], s0[1], a]
         agent.q[s0[0], s0[1], a] = agent.get_q(s0, a) + alpha * (r + gamma * agent.max_q(env.state) - agent.get_q(s0, a))
-        #if r == 0:
-        if is_goal:
-            break
     return i, R
         
 def actor_critic(env, agent, alpha, gamma):
@@ -158,7 +153,6 @@ def actor_critic(env, agent, alpha, gamma):
     i = 0
     r = -1
     is_goal = False
-    #while r != 0:
     while not is_goal:
         i += 1
         s0 = env.state.copy()
@@ -167,42 +161,39 @@ def actor_critic(env, agent, alpha, gamma):
         R += r
         value = agent.value[s0[0], s0[1]]
         policy = agent.policy[s0[0], s0[1], a]
-        #env.map[s0[0], s0[1]] = a
         delta = r + gamma * agent.value[env.state[0], env.state[1]] - value
-        #print (i, s0, a, delta, env.state, value, agent.value[env.state[0], env.state[1]], policy, agent.policy[env.state[0], env.state[1], a])
-        #print (s0, value, agent.value[s0[0], s0[1]], delta, policy, agent.policy[s0[0], s0[1], a])
         agent.value[s0[0], s0[1]] += delta# * alpha
         agent.policy[s0[0], s0[1], a] += delta# * alpha
         if is_goal:
             break
-    env.show_map()
-    print (i, R)
+    #env.show_map()
     return i, R
 ###
 if __name__ == '__main__':
 
     epsylon     = 0.1
-    alpha       = 0.1
+    alpha       = 0.5
     gamma       = 1.0
     dim         = (10, 7)
-    num         = 500
+    num         = 1000
 
     env = Env()
-    agent = Agent(epsylon)
-    ql_agent = Agent(epsylon)
+    agent       = Agent(epsylon)
+    ql_agent    = Agent(epsylon)
     ac_agent    = ActorCriticAgent(dim, epsylon)
     w = []
+    s_step_list = []
     for n in range(num):
-
-        s_step, s_r     = sarsa(env, agent, alpha, gamma)
+        s_step, s_r, s_a     = sarsa(env, agent, alpha, gamma)
         ql_step, ql_r   = q_learn(env, ql_agent, alpha, gamma)
         ac_step, ac_r   = actor_critic(env, ac_agent, alpha, gamma)
         w.append([n + 1, s_step, s_r, ql_step, ql_r, ac_step, ac_r])
+        s_step_list.append(s_step)
+        print ('%3d %.2f' % (s_step, np.array(s_step_list[-10:]).mean()), s_a)
 
     df = pd.DataFrame(w, columns = ['episode_id', 's_step', 's_r', 'ql_step', 'ql_r', 'ac_step', 'ac_r'])
     df['smoothed_s_r'] = smooth.smooth(np.array(df['s_r']), window_len=20)[:num]
     df['smoothed_ql_r'] = smooth.smooth(np.array(df['ql_r']), window_len=20)[:num]
     df['smoothed_ac_r'] = smooth.smooth(np.array(df['ac_r']), window_len=20)[:num]
-    #print (df)
     df.to_excel('windyworld.xlsx', index=False)
     #df.to_csv('windyworld.csv', index=False)
