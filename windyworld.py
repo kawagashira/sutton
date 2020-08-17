42e6f06d456384964afdeaf735b1fcf3a6c0e5d6
kawagashira <kawagashira@gmail.com>
2020-08-17 15:53:10 +0900

    heatmap and graph of predicted action arrow

--- windyworld.py
+++ windyworld.py
@@ -3,12 +3,19 @@
 #                            windyworld.py
 #
 
+import os
+import shutil
 import random
 import numpy as np
 import pandas as pd
 import smooth
+import matplotlib
+matplotlib.rcParams['backend'] = 'TkAgg'
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-DIRECTION = {0: 'U', 1:'R', 2:'D', 3:'L'}
+DIRECTION   = {0: 'U', 1:'R', 2:'D', 3:'L'}
+ARROW       = {0: '^', 1:'>', 2:'v', 3:'<'}
 
 class Env:
 
@@ -89,6 +96,20 @@ class Agent:
 
         return self.q[s[0], s[1], a]
 
+    def show_value(self, png_file):
+
+        m = np.max(self.q, axis=2)
+        sns.heatmap(m.transpose())
+        plt.savefig(png_file)
+        plt.close('all')
+
+    def show_arrow(self):
+
+        m = np.argmax(self.q, axis=2)
+        arrow = list(map(lambda x: ''.join([ARROW[x] for x in x]), m.transpose()))
+        for a in reversed(arrow):
+            print (a)
+
 class ActorCriticAgent:
 
     def __init__(self, dim, epsylon):
@@ -166,8 +187,8 @@ def actor_critic(env, agent, alpha, gamma):
         agent.policy[s0[0], s0[1], a] += delta# * alpha
         if is_goal:
             break
-    #env.show_map()
     return i, R
+
 ###
 if __name__ == '__main__':
 
@@ -176,6 +197,7 @@ if __name__ == '__main__':
     gamma       = 1.0
     dim         = (10, 7)
     num         = 1000
+    png_dir     = 'png'
 
     env = Env()
     agent       = Agent(epsylon)
@@ -183,17 +205,33 @@ if __name__ == '__main__':
     ac_agent    = ActorCriticAgent(dim, epsylon)
     w = []
     s_step_list = []
+
+    if os.path.isdir(png_dir):
+        shutil.rmtree(png_dir)
+    os.mkdir(png_dir)
+
     for n in range(num):
         s_step, s_r, s_a     = sarsa(env, agent, alpha, gamma)
         ql_step, ql_r   = q_learn(env, ql_agent, alpha, gamma)
         ac_step, ac_r   = actor_critic(env, ac_agent, alpha, gamma)
         w.append([n + 1, s_step, s_r, ql_step, ql_r, ac_step, ac_r])
         s_step_list.append(s_step)
-        print ('%3d %.2f' % (s_step, np.array(s_step_list[-10:]).mean()), s_a)
+        print ('%3d %3d %.2f' % (n+1, s_step, np.array(s_step_list[-10:]).mean()), s_a)
+        if (n+1) % 10 == 0:
+            png_file = '%s/value-%03d.png' % (png_dir, n+1)
+            agent.show_value(png_file)
+            agent.show_arrow()
+            plt.plot(s_step_list)
+            #plt.show()
+            step_list_file = '%s/step_list.png' % png_dir
+            plt.savefig(step_list_file)
+            plt.close('all')
 
+
+    """
     df = pd.DataFrame(w, columns = ['episode_id', 's_step', 's_r', 'ql_step', 'ql_r', 'ac_step', 'ac_r'])
     df['smoothed_s_r'] = smooth.smooth(np.array(df['s_r']), window_len=20)[:num]
     df['smoothed_ql_r'] = smooth.smooth(np.array(df['ql_r']), window_len=20)[:num]
     df['smoothed_ac_r'] = smooth.smooth(np.array(df['ac_r']), window_len=20)[:num]
-    df.to_excel('windyworld.xlsx', index=False)
     #df.to_csv('windyworld.csv', index=False)
+    """
